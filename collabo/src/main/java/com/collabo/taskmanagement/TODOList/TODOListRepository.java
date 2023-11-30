4ef8712e57a76966dcda9b6721674c5bcbe21cdd
ckdals9737 <ckdals9737@gmail.com>
2023-11-30 09:59:01 +0900

    11/30

--- collabo/src/main/java/com/collabo/taskmanagement/TODOList/TODOListRepository.java
+++ collabo/src/main/java/com/collabo/taskmanagement/TODOList/TODOListRepository.java
@@ -7,5 +7,7 @@ import java.util.List;
 @Mapper
 public interface TODOListRepository {
     public List<TODOList> list(int cIdx);
+    public List<TODOList> mylist(int idx);
+    public List<TODOList> list();
     public void update(int M_idx, int state, int idx);
 }
