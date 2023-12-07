514859073544f970b7df584912d6c32334a039bc
vitaminaddiction <leehaejun23@naver.com>
2023-12-07 13:27:46 +0900

    디테일 수정

--- collabo/src/main/java/com/collabo/taskmanagement/main/MainController.java
+++ collabo/src/main/java/com/collabo/taskmanagement/main/MainController.java
@@ -1,13 +1,21 @@
 package com.collabo.taskmanagement.main;
 
 
+import com.collabo.taskmanagement.auth.AuthService;
+import com.collabo.taskmanagement.auth.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @Autowired
+    AuthService authService;
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        Member member = authService.loadUserByAuthority();
+        model.addAttribute("member", member);
         return "main/index";
     }
 
